cd45045e3b2f7eee7639009ac2286b0e5e2e3574
MDBA <artes.mike@gmail.com>
2020-03-07 17:23:56 -0500

    resetting intial directory layout

--- src/App.js
+++ src/App.js
@@ -2,40 +2,39 @@ import React, { Component } from "react";
 import EmployeeCard from "./components/EmployeeCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import employees from "./employee.json"
+import employees from "./employee.json";
+
+
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     employees
   };
-
   removeEmployee = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const employees = this.state.employees.filter(employees => employees.id !== id);
+    // Filter this.state.friends for friends with an id not equal to the id being removed    
+    const employees = this.state.employees.filter(employee => employee.id !== id);
     // Set this.state.friends equal to the new friends array
     this.setState({ employees });
   };
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title>Employee List</Title>
-        {this.state.employees.map(employees => (
+        {this.state.employees.map(employee => (
           <EmployeeCard
-            removeEmployee={this.remove}
-            id={employees.id}
-            key={employees.id}
-            name={employees.name}
-            image={employees.image}
-            occupation={employees.occupation}
-            location={employees.location}
+            removeEmployee={this.removeEmployee}
+            id={employee.id}
+            key={employee.id}
+            firstname={employee.firstname}
+            lastname={employee.lastname}
+            image={employee.image}
+            occupation={employee.occupation}
+            location={employee.location}
           />
         ))}
       </Wrapper>
     );
   }
 }
-
 export default App;
--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./style.css";
-
 function EmployeeCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+        <img alt={props.firstname} src={props.image} />
       </div>
       <div className="content">
         <ul>
           <li>
-            <strong>Name:</strong> {props.name}
+            <strong>First:</strong> {props.firstname}
+          </li>
+          <li>
+            <strong>Last:</strong> {props.lastname}
           </li>
           <li>
             <strong>Occupation:</strong> {props.occupation}
@@ -20,11 +22,10 @@ function EmployeeCard(props) {
           </li>
         </ul>
       </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
+      <span onClick={() => props.removeEmployee(props.id)} className="remove">
         𝘅
       </span>
     </div>
   );
 }
-
 export default EmployeeCard;
